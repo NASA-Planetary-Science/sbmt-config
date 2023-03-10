81d50b34d1d6075b4774c11b48671ffb60e04035
James Peachey <jpeachey@users.noreply.github.com>
2023-03-10 16:59:44 -0500

    RM 2523: remove suffix from model metadata directory.

--- src/edu/jhuapl/sbmt/config/BasicConfigInfo.java
+++ src/edu/jhuapl/sbmt/config/BasicConfigInfo.java
@@ -33,7 +33,7 @@ public class BasicConfigInfo implements MetadataManager
 
     public static String getConfigPathPrefix(boolean publishedDataOnly)
     {
-        return (publishedDataOnly ? "published/" : "proprietary/") + "allBodies-" + configInfoVersion + "-redmine-2523";
+        return (publishedDataOnly ? "published/" : "proprietary/") + "allBodies-" + configInfoVersion;
     }
 
     private static final Mission[] EmptyMissionArray = new Mission[0];
